94ee00f2a86f830a02b2b98bccb7b1a54073b62f
박지수 <jisupark2000@gmail.com>
2024-02-21 20:55:56 +0900

    ✨ feat: 코디 생성기 페이지 - 스와이프로 이미지 바꾸는 기능 추가

--- app/cody/page.tsx
+++ app/cody/page.tsx
@@ -1,11 +1,25 @@
+'use client';
+
 import AppLayout from '@/components/layouts/appLayout';
 import PrevBtn from '@/components/navbar/prevBtn';
 import TopNavbar from '@/components/navbar/topNavbar';
+import ModelImgsBanner from '@/components/pages/cody/modelImgsBanner';
+import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Cody() {
+  const searchParams = useSearchParams();
+  const [modelImgs, setModelImgs] = useState<string[]>(searchParams.get('images')?.split(',') || []);
+  const [currentModelImgIdx, setCurrentModelImgIdx] = useState(0);
+  const [selectedClothImg, setSelectedClothImg] = useState<string>('');
+
   return (
     <AppLayout tnb={<TopNavbar left={<PrevBtn />} title='코디 실험실' />} showBNB={false}>
-      <div>코디 실험실</div>
+      <ModelImgsBanner
+        modelImgs={modelImgs}
+        currentImgIdx={currentModelImgIdx}
+        setCurrentImgIdx={setCurrentModelImgIdx}
+      />
     </AppLayout>
   );
 }
