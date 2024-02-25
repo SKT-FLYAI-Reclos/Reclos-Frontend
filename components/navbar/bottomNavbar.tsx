a9300f81566e03df5f2835dccb3ad8931a0f8f46
박지수 <jisupark2000@gmail.com>
2024-02-25 21:51:40 +0900

    💄 style: Bottom navbar 스타일 변경

--- components/navbar/bottomNavbar.tsx
+++ components/navbar/bottomNavbar.tsx
@@ -8,29 +8,58 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import cls from '@/libs/cls';
 
+type TNavItem = 'home' | 'closet' | 'chatting' | 'mypage';
+
+const navItems: TNavItem[] = ['home', 'closet', 'chatting', 'mypage'];
+
 export default function BottomNavbar() {
   const pathname = usePathname();
   return (
-    <nav className='fixed bottom-0 w-full h-70 bg-white border-t-1 border-t-gray-300 flex justify-center items-center gap-20'>
-      {navItems.map((navItem) => (
-        <NavItem
-          key={navItem.text}
-          icon={navItem.icon}
-          text={navItem.text}
-          to={navItem.to}
-          isActive={pathname === navItem.to}
-        />
-      ))}
+    <nav className='fixed bottom-0 w-full h-60 bg-white border-t-1 border-t-gray-300 flex justify-center items-center gap-20'>
+      {navItems.map((navItem) => getNavItem(pathname, navItem))}
     </nav>
   );
 }
 
-const navItems = [
-  { icon: <HomeIcon />, text: '홈', to: '/', isActive: true },
-  { icon: <ClosetIcon />, text: '옷장', to: '/closet', isActive: false },
-  { icon: <ChattingIcon />, text: '채팅', to: '/chatting', isActive: false },
-  { icon: <ProfileIcon />, text: 'My', to: '/mypage', isActive: false },
-];
+function getNavItem(pathname: string, icon: TNavItem) {
+  switch (icon) {
+    case 'home':
+      return (
+        <Link href={'/'} className='flex flex-col justify-center items-center gap-5 w-60 h-60'>
+          <HomeIcon fill={pathname === '/' ? 'black' : 'white'} stroke={pathname === '/' ? 'none' : '#9CA3AF'} />
+          <span className={cls('text-12', pathname === '/' ? 'text-black' : 'text-gray-400')}>홈</span>
+        </Link>
+      );
+    case 'closet':
+      return (
+        <Link href={'/closet'} className='flex flex-col justify-center items-center gap-5 w-60 h-60'>
+          <ClosetIcon stroke={pathname === '/closet' ? 'black' : '#9CA3AF'} />
+          <span className={cls('text-12', pathname === '/closet' ? 'text-black' : 'text-gray-400')}>옷장</span>
+        </Link>
+      );
+    case 'chatting':
+      return (
+        <Link href={'/chatting'} className='flex flex-col justify-center items-center gap-5 w-60 h-60'>
+          <ChattingIcon fill={pathname === '/chatting' ? 'black' : '#9CA3AF'} />
+          <span className={cls('text-12', pathname === '/chatting' ? 'text-black' : 'text-gray-400')}>채팅</span>
+        </Link>
+      );
+    case 'mypage':
+      return (
+        <Link href={'/mypage'} className='flex flex-col justify-center items-center gap-5 w-60 h-60'>
+          <ProfileIcon fill={pathname === '/mypage' ? 'black' : '#9CA3AF'} />
+          <span className={cls('text-12', pathname === '/mypage' ? 'text-black' : 'text-gray-400')}>My</span>
+        </Link>
+      );
+  }
+}
+
+// const navItems = [
+//   { icon: <HomeIcon fill='white' stroke='red' />, text: '홈', to: '/', isActive: true },
+//   { icon: <ClosetIcon />, text: '옷장', to: '/closet', isActive: false },
+//   { icon: <ChattingIcon />, text: '채팅', to: '/chatting', isActive: false },
+//   { icon: <ProfileIcon />, text: 'My', to: '/mypage', isActive: false },
+// ];
 
 function NavItem({ icon, text, to, isActive }: { icon: any; text: string; to: string; isActive: boolean }) {
   return (
